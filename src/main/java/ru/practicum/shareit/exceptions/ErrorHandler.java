5246e9805a5b79ce69bb6dc70809319a29416df2
Vova <vovasitnikov1209@yandex.ru>
2023-06-26 19:07:34 +0400

    вынос класса с ошибкой

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -39,15 +39,5 @@ public class ErrorHandler {
         return new ErrorResponse("Произошла непредвиденная ошибка");
     }
 
-     class ErrorResponse {
-        String error;
 
-        public ErrorResponse(String error) {
-            this.error = error;
-        }
-
-        public String getError() {
-            return error;
-        }
-    }
 }
