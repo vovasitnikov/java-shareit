75068c76e49014508a1e4f69126f6237f4a119e4
vovasitnikov <vovasitnikov1209@yandex.ru>
2023-06-27 10:42:03 +0400

    ее

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -17,7 +17,6 @@ import ru.practicum.shareit.utility.PageDefinition;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -35,8 +34,11 @@ public class BookingServiceImpl implements BookingService {
     @Transactional
     public BookingDto createBooking(Integer userId, BookingItemDto bookingItemDto) {
         checkBookingDates(bookingItemDto);
-        //User user = userService.getUserById(userId);
-        Optional<User> user = userRepository.findById(userId);
+        User user = userService.getUserById(userId);
+/*        User user = userRepository.findById(userId).orElseThrow(() -> {
+            log.warn("Пользователь не найден");
+            return new NotFoundException("Такой пользователь не найден");
+        });*/
         Item item = itemService.getItemById(bookingItemDto.getItemId());
         if (item.getOwner().equals(user)) {
             log.warn("Пользователь не может забронировать собственный предмет");
