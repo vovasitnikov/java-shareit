41a6e6158d4caa79ad0c88af976f967628632c2f
vovasitnikov <vovasitnikov1209@yandex.ru>
2023-06-27 15:51:22 +0400

    проба1

--- src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
@@ -63,7 +63,8 @@ class BookingServiceImplTest {
     @Test
     void createBooking_returnBooking() {
         BookingItemDto bookingItemDto = BookingMapper.toBookingItemDto(booking);
-        when(userService.getUserById(anyInt())).thenReturn(user2);
+        // when(userService.getUserById(anyInt())).thenReturn(user2);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.ofNullable(user2));
         when(itemService.getItemById(any())).thenReturn(item);
         when(bookingRepository.save(any())).thenReturn(booking);
 
@@ -76,7 +77,8 @@ class BookingServiceImplTest {
     @Test
     void createBooking_selfBooking_returnException() {
         BookingItemDto bookingItemDto = BookingMapper.toBookingItemDto(booking);
-        when(userService.getUserById(anyInt())).thenReturn(user1);
+        //when(userService.getUserById(anyInt())).thenReturn(user1);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.ofNullable(user1));
         when(itemService.getItemById(any())).thenReturn(item);
 
         assertThrows(
@@ -89,7 +91,8 @@ class BookingServiceImplTest {
     void createBooking_notAvailable_returnException() {
         item.setIsAvailable(false);
         BookingItemDto bookingItemDto = BookingMapper.toBookingItemDto(booking);
-        when(userService.getUserById(anyInt())).thenReturn(user2);
+        //when(userService.getUserById(anyInt())).thenReturn(user2);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.ofNullable(user2));
         when(itemService.getItemById(any())).thenReturn(item);
 
         assertThrows(
@@ -152,7 +155,8 @@ class BookingServiceImplTest {
     @Test
     void getBookingById_returnBookingDto() {
         when(bookingRepository.findById(anyInt())).thenReturn(Optional.of(booking));
-        when(userService.getUserById(anyInt())).thenReturn(user1);
+        //when(userService.getUserById(anyInt())).thenReturn(user1);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.ofNullable(user1));
 
         BookingDto newBookingDto = bookingService.getBookingById(1, 1);
 
@@ -162,7 +166,8 @@ class BookingServiceImplTest {
     @Test
     void getBookingById_wrongUser_returnException() {
         when(bookingRepository.findById(anyInt())).thenReturn(Optional.of(booking));
-        when(userService.getUserById(anyInt())).thenReturn(user1);
+        //when(userService.getUserById(anyInt())).thenReturn(user1);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.ofNullable(user1));
 
         NotFoundException repeatBooking = assertThrows(
                 NotFoundException.class,
@@ -178,7 +183,8 @@ class BookingServiceImplTest {
         PageRequest page = PageRequest.of(from, size);
         Page<Booking> bookingPage = new PageImpl<>(bookingList);
 
-        when(userService.getUserById(anyInt())).thenReturn(user1);
+        //when(userService.getUserById(anyInt())).thenReturn(user1);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.ofNullable(user1));
         when(bookingRepository.findAllByBookerIdOrderByStartDesc(anyInt(), eq(page))).thenReturn(bookingPage);
         when(bookingRepository.findAllByBookerIdAndStartBeforeAndEndAfterOrderByStartDesc(anyInt(), any(), any(), eq(page))).thenReturn(bookingPage);
         when(bookingRepository.findAllByBookerIdAndEndBeforeOrderByStartDesc(anyInt(), any(), eq(page))).thenReturn(bookingPage);
@@ -218,7 +224,8 @@ class BookingServiceImplTest {
         PageRequest page = PageRequest.of(from, size);
         Page<Booking> bookingPage = new PageImpl<>(bookingList);
 
-        when(userService.getUserById(anyInt())).thenReturn(user1);
+        //when(userService.getUserById(anyInt())).thenReturn(user1);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.ofNullable(user1));
         when(bookingRepository.getBookingsForOwner(anyInt(), eq(page))).thenReturn(bookingPage);
         when(bookingRepository.getBookingsForOwnerCurrent(anyInt(), any(), any(), eq(page))).thenReturn(bookingPage);
         when(bookingRepository.getBookingsForOwnerPast(anyInt(), any(), eq(page))).thenReturn(bookingPage);
