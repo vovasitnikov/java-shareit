21b3683789957fa0c5ee48f37d3de9970074fa82
vovasitnikov <vovasitnikov1209@yandex.ru>
2023-06-28 09:35:53 +0400

    добавил аннотацию

--- src/main/java/ru/practicum/shareit/item/comment/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/comment/CommentDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -15,7 +16,7 @@ public class CommentDto {
 
     private Integer id;
 
-
+    @NotNull
     private String text;
 
     private String authorName;
